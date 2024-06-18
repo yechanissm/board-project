b60256e630e3d4f76919c77094ca1967e19b7d74
yechanissm <106381504+yechanissm@users.noreply.github.com>
2024-06-18 22:43:58 +0900

    #15 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/com/board/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/board/boardproject/controller/ArticleControllerTest.java
@@ -35,7 +35,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
