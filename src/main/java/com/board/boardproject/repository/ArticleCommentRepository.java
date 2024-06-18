e3dd1d2c30a2822e10b560ffbd30ae15d00a2ce0
yechanissm <106381504+yechanissm@users.noreply.github.com>
2024-06-18 13:19:55 +0900

    #1 - Quertdsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslBinderCustomizer`,
    `QuerydslPredicateExecutor`,를 이용하여 간단하게 개발

--- src/main/java/com/board/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/com/board/boardproject/repository/ArticleCommentRepository.java
@@ -1,9 +1,34 @@
 package com.board.boardproject.repository;
 
+import com.board.boardproject.domain.Article;
 import com.board.boardproject.domain.ArticleComment;
+import com.board.boardproject.domain.QArticle;
+import com.board.boardproject.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        /**
+         * StringExpression::containsIgnoreCase는 like '%${v}%'
+         * StringExpression::likeIgnoreCase는 like '${v}'
+         */
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
